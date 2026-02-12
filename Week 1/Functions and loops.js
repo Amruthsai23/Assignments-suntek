a9bedf5d4b42510dd501ac42a473da55ddb2e9ac
Amruthsai23 <23eg112e64@anurag.edu.in>
2026-02-12 18:02:12 +0530

    intial commit

---
+++ Week 1/Functions and loops.js
@@ -0,0 +1,52 @@
+// write a function thar recevies marks array as argument and returns small element
+
+// marks = [20,40,56,78,20]
+// let min = marks[0]
+// function check_min(a){
+//     for(let m of a){
+//         if(min>m)
+//             min = m
+//     }
+//     return min;
+// } 
+
+// let res = check_min(marks);
+// console.log(res);
+
+//-------------------------------------------------------------------------------------------
+// // check wheather the skill is present or not in skilArr
+// for of loop is used to iterate over elements
+// let skillArr = ['java','c','c++','python']
+// function findSkill(skillArr1,skill){
+//     for(let v  of skillArr1){
+//         if(v === skill){
+//             return true;
+//         }
+//     }
+//     return false
+// }
+// let res = findSkill(skillArr,'java')
+// console.log(res);
+//-------------------------------------------------------------------------------------------
+// object group of properties
+// for in loop iterate over keys
+// let empObj = {
+//     empNo:100,
+//     name:'ram',
+//     age:89,
+// }
+// for(let i in empObj){
+//     console.log(i, "is",empObj[i])
+// }
+
+let stuObj={
+    rollNo : 10,
+    name :'charan',
+    city : 'hyd',
+}
+
+let proObj={
+    proName : 'pen',
+    brand : 'cello',
+    price : 100,
+}
